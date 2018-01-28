f53015e369b70037a08a47f13b46ea56c947abe7
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-01-28 00:04:48 -0600

    U1A1

---
+++ Unit 1/U1A1/src/U1A1.java
@@ -0,0 +1,29 @@
+// Dominic Rutkowski
+//
+/* This project prints
+   out a face.
+*/
+
+public class U1A1
+{
+	public static void main(String[] args)
+	{
+		// Prints hair
+		System.out.println("  /////  ");
+		
+		// Prints forehead
+		System.out.println(" |     | ");
+		
+		// Prints eyebrows (uses \")
+		System.out.println(" | \" \" | ");
+		
+		// Prints eyes
+		System.out.println(" | o o | ");
+		
+		// Prints ears and nose
+		System.out.println("(|  ^  |)");
+		
+		// Prints mouth and chin (uses \\ and \n)
+		System.out.print(" | \\_/ |\n  -----  \n");
+	}
+}

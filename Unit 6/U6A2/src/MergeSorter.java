10d7c2ed539f9cff7a19799ac6580abb7806209a
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-02-02 11:29:45 -0600

    Worked on U6A2

--- Unit 6/U6A2/src/MergeSorter.java
+++ Unit 6/U6A2/src/MergeSorter.java
@@ -6,4 +6,33 @@
 
 public class MergeSorter
 {
+	private int[] arr;
+
+	public MergeSorter(int[] arr)
+	{
+		this.arr = arr;
+	}
+
+	public void sort()
+	{
+		if (arr.length <= 1)
+		{
+			return;
+		}
+		int[] first = new int[arr.length / 2];
+		int[] second = new int[arr.length - first.length];
+		System.arraycopy(arr, 0, first, 0, first.length);
+		System.arraycopy(arr, first.length, second, 0, second.length);
+		MergeSorter firstSorter = new MergeSorter(first);
+		MergeSorter secondSorter = new MergeSorter(second);
+
+		firstSorter.sort();
+		secondSorter.sort();
+		merge(first, second);
+	}
+
+	private void merge(int[] first, int[] second)
+	{
+
+	}
 }

6ec6958c19bafead0da800fa75fe0ef10b5e9875
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-02-07 11:30:28 -0600

    Worked on Unit 6 Free Response

--- Unit 6/Unit 6 Free Response/src/Searcher.java
+++ Unit 6/Unit 6 Free Response/src/Searcher.java
@@ -21,6 +21,27 @@ public class Searcher
 
 	public boolean binSearch(int t)
 	{
-
+		int low = 0;
+		int high = list.length - 1;
+		int difference;
+		int middle;
+		while (low <= high)
+		{
+			middle = (low + high) / 2;
+			difference = list[middle] - t;
+			if (difference == 0)
+			{
+				return true;
+			}
+			else if (difference < 0)
+			{
+				low = middle + 1;
+			}
+			else
+			{
+				high = middle - 1;
+			}
+		}
+		return false;
 	}
 }

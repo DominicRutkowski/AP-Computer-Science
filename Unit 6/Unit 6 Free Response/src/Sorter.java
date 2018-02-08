42515c63aca69fe5b98a20ac70722e76c79e8010
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-02-08 10:57:39 -0600

    Worked on Unit 6 Free Response

--- Unit 6/Unit 6 Free Response/src/Sorter.java
+++ Unit 6/Unit 6 Free Response/src/Sorter.java
@@ -9,21 +9,50 @@ public class Sorter
 
 	public void selectionSort()
 	{
-
+		int minimumPosition;
+		int temp;
+		for (int j = 0; j < list.length - 1; j++)
+		{
+			minimumPosition = j;
+			for (int k = j + 1; k < list.length; k++)
+			{
+				if (list[k] < list[minimumPosition])
+				{
+					minimumPosition = k;
+				}
+				temp = list[j];
+				list[j] = list[minimumPosition];
+				list[minimumPosition] = temp;
+			}
+		}
 	}
 
 	public void insertionSort()
 	{
-
+		int j;
+		int temp;
+		for (int n = 1; n < list.length; n++)
+		{
+			temp = list[n];
+			j = n;
+			while (j > 0 && temp < list[j - 1])
+			{
+				list[j] = list[j - 1];
+				j--;
+			}
+			list[j] = temp;
+		}
 	}
 
 	public void mergeSort()
 	{
-
+		MergeSorter merge = new MergeSorter(list);
+		merge.sort();
 	}
 
 	public void quickSort()
 	{
-
+		QuickSorter quick = new QuickSorter(list);
+		quick.sort(0, list.length - 1);
 	}
 }

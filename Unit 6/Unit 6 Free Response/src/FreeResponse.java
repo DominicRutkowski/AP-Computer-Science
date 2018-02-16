bc7cc2d01a275f883e4e1606a54c09a9ba39f4bd
Dominic Rutkowski <rutkowski.dominic@gmail.com>
2018-02-16 11:27:21 -0600

    Finished Unit 6 Free Response

--- Unit 6/Unit 6 Free Response/src/FreeResponse.java
+++ Unit 6/Unit 6 Free Response/src/FreeResponse.java
@@ -7,6 +7,25 @@ public class FreeResponse
 	private FreeResponse()
 	{
 		fillArray();
+		final int target = 1234;
+		int[] secondAry = ary.clone();
+
+		long aryStart = System.currentTimeMillis();
+		Searcher arySearch = new Searcher(ary);
+		boolean foundAry = arySearch.seqSearch(target);
+		long aryStop = System.currentTimeMillis();
+		int aryTime = (int) (aryStop - aryStart);
+
+		long secondAryStart = System.currentTimeMillis();
+		Sorter secondArySort = new Sorter(secondAry);
+		secondArySort.mergeSort();
+		Searcher secondArySearch = new Searcher(secondAry);
+		boolean foundSecondAry = secondArySearch.binSearch(target);
+		long secondAryStop = System.currentTimeMillis();
+		int secondAryTime = (int) (secondAryStop - secondAryStart);
+
+		System.out.println("Idea #1:  Found = " + foundAry + "\t\t" + "Time = " + aryTime);
+		System.out.println("Idea #2:  Found = " + foundSecondAry + "\t\t" + "Time = " + secondAryTime);
 	}
 
 	public static void main(String[] args)

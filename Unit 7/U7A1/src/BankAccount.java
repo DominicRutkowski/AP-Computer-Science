8bbaf8f792f3de56581be48c13e38167abd2b2f0
DominicRutkowski <rutkowski.dominic@gmail.com>
2018-02-21 22:50:53 -0600

    Nearly finished U7A1

--- Unit 7/U7A1/src/BankAccount.java
+++ Unit 7/U7A1/src/BankAccount.java
@@ -1,6 +1,7 @@
 // Dominic Rutkowski
 //
-/*
+/* This class represents bank accounts. It allows for
+   deposits, withdrawals, interest accumulation, etc.
 */
 
 import java.util.Random;
@@ -10,12 +11,20 @@ public class BankAccount
 	private double balance;
 	private int accountNumber;
 	private static int lastAssignedAccountNumber = 100;
+	private static final double interestRate = 4;
 
 	public BankAccount()
 	{
 		lastAssignedAccountNumber++;
 		accountNumber = lastAssignedAccountNumber;
 		setBalance(1000);
+	}
+
+	public BankAccount(double balance)
+	{
+		lastAssignedAccountNumber++;
+		accountNumber = lastAssignedAccountNumber;
+		this.balance = balance;
 	}
 
 	private void setBalance(int max)
@@ -28,5 +37,55 @@ public class BankAccount
 		}
 		while (balance == 0);
 		this.balance = balance;
+	}
+
+	public String deposit(double amount)
+	{
+		if (amount > 0)
+		{
+			balance += amount;
+			return "Deposit Accepted";
+		}
+		return "Deposit Rejected";
+	}
+
+	public String withdraw(double amount)
+	{
+		if (amount <= balance)
+		{
+			balance -= amount;
+			return "Withdrawal Accepted";
+		}
+		return "Withdrawal Denied - Insufficient Funds";
+	}
+
+	public void calculateInterest()
+	{
+		balance *= 1 + (interestRate / 100);
+	}
+
+	public String transaction(BankAccount account, double amount)
+	{
+		if (withdraw(amount).equals("Withdrawal Accepted"))
+		{
+			account.deposit(amount);
+			return "Transaction Accepted";
+		}
+		return "Transaction Denied - Insufficient Funds";
+	}
+
+	public int getAccountNumber()
+	{
+		return accountNumber;
+	}
+
+	public String getBalance()
+	{
+		return String.format("$ %.2f", balance);
+	}
+
+	public static String getInterestRate()
+	{
+		return String.format("%.1f", interestRate);
 	}
 }
--- Unit 7/U7A1/src/U7A1.java
+++ Unit 7/U7A1/src/U7A1.java
@@ -1,14 +1,48 @@
 // Dominic Rutkowski
 //
-/*
+/* This driver class simulates the change
+   in balance of two BankAccounts.
 */
 
+import javax.swing.JTextArea;
 import javax.swing.JApplet;
+import java.awt.Container;
+import java.awt.Font;
 
 public class U7A1 extends JApplet
 {
 	public void init()
 	{
+		JTextArea text = new JTextArea();
+		text.setFont(new Font("Monospaced", Font.PLAIN, 12));
+		text.setText("Account Information\n-------------------\n");
 
+		BankAccount accountOne = new BankAccount();
+		BankAccount accountTwo = new BankAccount(1500);
+
+		text.append("\nInitial Balance for Account " + accountOne.getAccountNumber() + " = " + accountOne.getBalance());
+		text.append("\nInitial Balance for Account " + accountTwo.getAccountNumber() + " = " + accountTwo.getBalance() + "\n");
+		text.append("\n" + accountOne.deposit(300));
+		text.append("\n" + accountTwo.deposit(200));
+		text.append("\n" + accountOne.withdraw(250));
+		text.append("\n" + accountTwo.withdraw(125));
+		accountOne.calculateInterest();
+		accountTwo.calculateInterest();
+		if (accountOne.transaction(accountTwo, 550).equals("Transaction Accepted"))
+		{
+			text.append("\nWithdrawal Accepted\nDeposit Accepted");
+		}
+		else
+		{
+			text.append("\nWithdrawal Denied - Insufficient Funds\nDeposit Rejected");
+		}
+		accountOne.calculateInterest();
+		accountTwo.calculateInterest();
+		text.append("\n\nFinal Balance for Account " + accountOne.getAccountNumber() + " = " + accountOne.getBalance());
+		text.append("\n\nFinal Balance for Account " + accountTwo.getAccountNumber() + " = " + accountTwo.getBalance());
+		text.append("\n\nInterest Rate = " + BankAccount.getInterestRate() + " %");
+
+		Container cont = getContentPane();
+		cont.add(text);
 	}
 }

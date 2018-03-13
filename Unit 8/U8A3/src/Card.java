023ce413ed524832b04f191459b2ae98572c2c53
DominicRutkowski <rutkowski.dominic@gmail.com>
2018-03-13 14:24:45 -0400

    Finished U8A3

---
+++ Unit 8/U8A3/src/Card.java
@@ -0,0 +1,82 @@
+// Dominic Rutkowski
+/*
+*/
+
+public class Card implements Comparable
+{
+    private String suit;
+    private int value;
+    private int points;
+
+    public Card(String suit, int value)
+    {
+        this.suit = suit;
+        this.value = value;
+        setPoints();
+    }
+
+    private void setPoints()
+    {
+        if (suit.equals("Hearts"))
+        {
+            points = 1;
+        }
+        if (suit.equals("Spades") && value == 12)
+        {
+            points = 13;
+        }
+    }
+
+    public int compareTo(Object object) throws IllegalArgumentException
+    {
+        if (object instanceof Card)
+        {
+            Card card = (Card) object;
+            return this.getValue() - card.getValue();
+        }
+        else
+        {
+            throw new IllegalArgumentException("Must pass another Card object.");
+        }
+    }
+
+    public String getSuit()
+    {
+        return suit;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+
+    public int getPoints()
+    {
+        return points;
+    }
+
+    public String toString()
+    {
+        String card;
+        switch (value)
+        {
+            case 14:
+                card = "Ace";
+                break;
+            case 13:
+                card = "King";
+                break;
+            case 12:
+                card = "Queen";
+                break;
+            case 11:
+                card = "Jack";
+                break;
+            default:
+                card = Integer.toString(value);
+                break;
+        }
+        card += " of " + suit;
+        return card;
+    }
+}
